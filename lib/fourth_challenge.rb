72c31fbf5c89a8a4b4c0df22408bb29fdde3ae48
John Wojciechowski <jwojo13@gmail.com>
2017-11-20 20:21:10 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,8 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         benvolio = {
-          :name => "Benvolio"
-          :age => "17"
+          :name => "Benvolio",
+          :age => "17",
           :attitude => "worried"
         }
       ]
