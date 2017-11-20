352f2b429278b8101a6d6396b31fb9aeb336e15b
John Wojciechowski <jwojo13@gmail.com>
2017-11-20 20:18:21 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,11 +2,11 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Mantague",
+        :name => "Lord Montague",
         :age => "53"
         },
       :matriarch => {
-        :name => "Lady Mantague",
+        :name => "Lady Montague",
         :age => "54"
         },
       :hero => {
