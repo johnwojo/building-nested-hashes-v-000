a79355b5d3a07033e4403d0d54bf0d7aba6cb198
John Wojciechowski <jwojo13@gmail.com>
2017-11-20 20:21:54 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,7 +9,13 @@ def fourth_challenge
           :name => "Benvolio",
           :age => "17",
           :attitude => "worried"
+        },
+        mercutio = {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
+
       ]
    },
    :capulet => {
