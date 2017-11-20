c776ef32308ab156dde3fd9678945f19caf2e93d
John Wojciechowski <jwojo13@gmail.com>
2017-11-20 20:23:06 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -22,7 +22,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        steven = {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        },
+        nurse = {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        }
       ]
    }
 }
